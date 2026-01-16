17866ea5ffe5c40b7fdb8d03a3d91068c8e067a9
Shi Yong <xtor369@gmail.com>
2026-01-16 17:19:05 +0800

    Level 0

---
+++ src/main/java/Artemis.java
@@ -0,0 +1,19 @@
+
+public class Artemis {
+    public static void main(String[] args) {
+        String logo = """
+             _         _                 _     
+            / \\   _ __| |_ ___ _ __ ___ (_)___ 
+           / _ \\ | '__| __/ _ \\ '_ ` _ \\| / __|
+          / ___ \\| |  | ||  __/ | | | | | \\__ \\
+         /_/   \\_\\_|   \\__\\___|_| |_| |_|_|___/
+                                               
+        """;
+        System.out.println("Hello from\n" + logo);
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Artemis\nWhat can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
